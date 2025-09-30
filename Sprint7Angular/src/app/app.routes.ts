3a8933f3da2fd9cbcb2d6bb1192e86ab1195eb3b
bispo-eugenio <bispoeugenio270@gmail.com>
2025-09-30 07:46:03 -0300

    feature: adiciona guard no dashboard

--- Sprint7Angular/src/app/app.routes.ts
+++ Sprint7Angular/src/app/app.routes.ts
@@ -8,5 +8,5 @@ export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'',redirectTo:'login',pathMatch:'full'},
     {path:'home',component:HomeComponent,canActivate:[authGuard]},
-    {path:'dashboard', component:DashboardComponent}
+    {path:'dashboard', component:DashboardComponent, canActivate:[authGuard]}
 ];
